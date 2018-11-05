d63aca2e5ed80eb40b7aae14b8b8a86f969bf80f
lvlaxjh <2876363140@qq.com>
2018-11-05 17:07:22 +0800

    0.01.2.4

--- Myalgorithm.py
+++ Myalgorithm.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-#-------------------------------------------------------------------------#
-#KMP算法#
-#传参:字符串,字符串
-#返回:正确匹配次数
+# -------------------------------------------------------------------------
+# KMP algorithm
+# 传参:字符串(str),字符串(str)
+# 返回:正确匹配次数(int)
 def KMP_next(_str):
     # 初始化
     _j = []
@@ -34,6 +34,8 @@ def KMP_next(_str):
     for i in str:
         str_l.append(i)
     return _j, str_l, _next
+
+
 def KMP(_strF, _strS):
     # 初始化
     strF = ''
@@ -83,4 +85,102 @@ def KMP(_strF, _strS):
 
     print(same)
     return same
-#-------------------------------------------------------------------------#
+
+
+# -------------------------------------------------------------------------
+# 栈#
+class Stack:
+    def __init__(self):
+        self.__stack = []
+        pass
+
+    # 入栈
+    # 传参:入栈内容(all)
+    # 返回:栈(list)
+    def Push(self, Content):
+        self.__stack.append(Content)
+        return self.__stack
+
+    # 出栈
+    # 传参:无
+    # 返回:出栈内容(all)/若栈为空:返回False(bool)
+    def Pop(self):
+        if self.__stack == []:
+            return False
+        this_pop = self.pop
+        return this_pop
+
+    # 求栈长度
+    # 参数:无
+    # 返回:栈长度(int)
+    def Size(self):
+        size = len(self.__stack)
+        return size
+
+    # 清空栈
+    # 传参:无
+    # 返回:None
+    def Delete(self):
+        del self.__stack[:]
+        return None
+
+    # 完整栈
+    # 传参:无
+    # 返回:栈(list)
+    def Show(self):
+        return self.__stack
+
+
+# -------------------------------------------------------------------------
+# GCD algorithm
+# 传参:整数(int),整数(int)
+# 返回:最大公约数(int)
+# 相除法
+def Division_GCD(Num1, Num2):
+    if Num2 > Num1:
+        flag = Num1
+        Num1 = Num2
+        Num2 = flag
+    while (True):
+        Remainder = Num1 % Num2
+        if Remainder == 0:
+            return Num2
+        else:
+            Num1 = Num2
+            Num2 = Remainder
+
+
+# 相减法
+def Subtraction_GCD(Num1, Num2):
+    while (True):
+        if Num2 > Num1:
+            flag = Num1
+            Num1 = Num2
+            Num2 = flag
+        Num1 = Num1 - Num2
+        if Num1 == Num2:
+            return Num1
+
+
+# 穷举法
+def Exhaustive_GCD(Num1, Num2):
+    if Num2 > Num1:
+        flag = Num1
+        Num1 = Num2
+        Num2 = flag
+    flag = Num2
+    while (True):
+        if Num1 % flag == 0 and Num2 % flag == 0:
+            return flag
+        flag -= 1
+
+
+# -------------------------------------------------------------------------
+# LCM algorithm
+# 传参:整数(int),整数(int)
+# 返回:最小公倍数(int)
+def LCM(Num1, Num2):
+    return int((Num1 * Num2) / Division_GCD(Num1, Num2))
+
+# -------------------------------------------------------------------------
+# 24Point
