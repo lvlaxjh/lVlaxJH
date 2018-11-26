ce6f9a03b82f67db7cb92fb6fd2410ea460f546c
lvlaxjh <44161928+lvlaxjh@users.noreply.github.com>
2018-11-26 09:50:44 +0800

    0.01.4.2

--- python/Shield/Shield.py
+++ python/Shield/Shield.py
@@ -33,16 +33,21 @@ tree_bool = False
 T_bool = False
 
 #情况:
-#1.word短,不同
-#2.word相等长,不同
-#3.word相等长,相同
-#4.word长,基础部分不同
+#1.word短,不同->v
+#2.word相等长,不同->v
+#3.word相等长,相同->v
+#4.word长,基础部分不同->v
 #5.word长,基础部分相同
 def Common_Preorder_Traversal(deep,word, tree_List=[]):
     global tree_bool
     global T_bool
     this_tree_list=tree_List
     this_word = word
+    if len(this_word)>1 and deep == 1:  # 当word较长,且树只有根节点时
+        this_word=this_word[1:]
+        tree_List.append(Common_Tree(this_word))  # 新建节点,创造树
+        print('!')
+        return
     if this_word == '':
         return
     for i in this_tree_list:
@@ -60,15 +65,12 @@ def Common_Preorder_Traversal(deep,word, tree_List=[]):
             tree_bool = False
             print(1)
             return
-        if this_word != '' and deep == 1:#当word较长,且树只有根节点时
-            i.append(Common_Tree(this_word))  # 新建节点,创造树
-            print('!')
-            return
-        if this_word != '' and type(i) == list:#word的较长,树为完整树
-            i.append(Common_Tree(this_word))  # 新建节点,创造树
-            print('@')
-            return
 
+        # if this_word != '' and type(i) == list:#word的较长,树为完整树
+        #     i.append(Common_Tree(this_word))  # 新建节点,创造树
+        #     print('@')
+        #     return
+c=['1']
 a=['1', ['2', ['3']]]
 b=input()
 Common_Preorder_Traversal(len(a),b,a)
