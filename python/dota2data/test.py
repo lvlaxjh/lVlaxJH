227aa8294197f203da2d4dbeda0352cd380e41f6
lvlaxjh <44161928+lvlaxjh@users.noreply.github.com>
2018-12-17 20:26:27 +0800

    1

--- python/dota2data/test.py
+++ python/dota2data/test.py
@@ -1,52 +1,34 @@
 import pymysql
 
-db = pymysql.Connect(
-    host='localhost',
-    port=3306,
-    user='root',
-    passwd='jhczxcvbnm',
-    db='dota2data',
-    charset='utf8'
-)
+from sshtunnel import SSHTunnelForwarder
 
-
+with SSHTunnelForwarder(
+        ('47.93.36.54', 22),  # B机器的配置
+        ssh_password="jhczxcvbnm0325!!!",
+        ssh_username="root",
+        remote_bind_address=('127.0.0.1', 3306)) as server:  # A机器的配置
+    db = pymysql.Connect(
+        host='127.0.0.1',
+        port=3306,
+        user='root',
+        passwd='jhczxcvbnm',
+        db='dota2data',
+        charset='utf8'
+    )
 cursor=db.cursor()
 
-# SQL 插入语句
-sql ='''insert into dota2data(heroname,alluse,allwin,state,times) values ("%s","%s","%s","%s","%s")'''% \
-     ('1','2','3','4','6')
+dota2_data={'all':'dota2dataall',
+            'vh':'dota2datavh',
+            'pro':'dota2datapro'}
+sql = "SELECT * FROM %s" % dota2_data['pro']
 
-try:
-# 执行sql语句
-    cursor.execute(sql)
-    # 执行sql语句
-    db.commit()
-except:
-   # 发生错误时回滚
-   print('error')
-   db.rollback()
-
-# sql = "DELETE FROM `dota2data` WHERE (`heroname`='1') AND (`alluse`='2') AND (`allwin`='3') AND (`state`='4') AND (`times`='5') LIMIT 1"
-# try:
-#    # 执行SQL语句
-#    cursor.execute(sql)
-#    # 提交修改
-#    db.commit()
-# except:
-#    # 发生错误时回滚
-#    db.rollback()
-
-
-sql = "SELECT * FROM dota2data"
-
-try:
-   # 执行SQL语句
-   cursor.execute(sql)
-   # 获取所有记录列表
-   results = cursor.fetchall()
-
-   print(results)
-except:
-   print ("Error: unable to fecth data")
+# 执行SQL语句
+cursor.execute(sql)
+# 获取所有记录列表
+results = cursor.fetchall()
+all_data_tub=results
+for i in all_data_tub:
+    for n in i:
+        print(n)
 
 db.close()
